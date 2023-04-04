e13d4014e1d8d72089bc0d91d4a0e1575e4f884e
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-04-04 20:13:03 -0300

    I removed the logic to load peoples at the filter_people file and created a new function only for this purpose

---
+++ src/functions/loadPeople.ts
@@ -0,0 +1,16 @@
+import * as fs from 'fs'
+import * as path from 'path'
+
+import { Pessoa as People } from '../interfaces/generalInterfaces'
+
+/**
+ * Reads the people.json file and returns an array of Person objects.
+ */
+const loadPeople = (): People[] => {
+  const filePath = path.join(__dirname, '..', 'pessoas.json')
+  const peopleJson = fs.readFileSync(filePath, 'utf-8')
+  const peoples: People[] = JSON.parse(peopleJson)
+  return peoples
+}
+
+export default loadPeople
